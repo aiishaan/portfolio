5ffb31bafc3ba1ab0ee56d6b50c68c6e85468401
aishanirfan <aishanirfan6@gmail.com>
2024-06-05 13:56:42 +0530

    changed hero image

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css'
-import heroImg from '../../assets/hero-img.png'
+import heroImg from '../../assets/headShot.png'
 import sun from '../../assets/sun.svg'
 import moon from '../../assets/moon.svg'
 import gmailLight from '../../assets/gmail-light.svg'
